908d3c10fbbe7cfee3f7c737cac18b7b2f0b5ac0
romazpua <roma.zp.ua@gmail.com>
2021-02-07 20:54:28 +0200

    finish project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-jQuery(document).ready(function($) {});
+jQuery(document).ready(function ($) {
+
+
+    $("#uptotop").click(function () {
+        $("html, body").animate({scrollTop: 0}, "slow");
+        return false;
+    });
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 1000) {
+            $('#uptotop').fadeIn(500)
+        } else if ($(window).scrollTop() < 1000) {
+            $('#uptotop').fadeOut(500)
+        }
+    })
+
+
+});
